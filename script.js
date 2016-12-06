2346f7c1898927e921c831cbb5bdaf7758dfc0b4
hawasian <connorkyoung@outlook.com>
2016-12-06 17:50:14 +0000

    Rethink of Place. The start of a less star-y cloud

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 var minSize = .5; //em
 var maxSize = 3; //em
 
+var maxH = 0;
+var maxW = 0;
+
+var xOrigin;
+var yOrigin;
+
+
 $(document).ready(function() {
     $('#gen').click(function() {
         $('#wordcloud').html("");
@@ -63,71 +70,85 @@ function measure(inArray, maxVal) { //calculates the font-size, and the height, 
 
         var data = "<span id='word'style='font-size:" + row['font-size'] + "'>" + word + "</span>";
         $('#workArea').html(data);
-            row['width'] = $('#word').outerWidth();
-            row['height'] = $('#word').outerHeight();
+        row['width'] = $('#word').outerWidth();
+        row['height'] = $('#word').outerHeight();
+        maxW += row['width'];
+        maxH += row['height'];
 
 
         outArray.push(row);
     }
     $('#workArea').html("").hide();
+    maxW = maxW / 3;
+    maxH = maxH / 3;
     return outArray;
 }
 
-function position(inArray, wcCont){
-    var xOrigin = $(wcCont).width() / 2;
-    var yOrigin = $(wcCont).height() / 2;
-    inArray[0]["x"] = xOrigin - (inArray[0]["width"]/2);
-    inArray[0]["y"] = yOrigin - (inArray[0]["height"]/2);
+function position(inArray, wcCont) {
+    xOrigin = $(wcCont).width() / 2;
+    yOrigin = $(wcCont).height() / 2;
+    inArray = shuffle(inArray);
+    inArray[0]["x"] = xOrigin - (inArray[0]["width"] / 2);
+    inArray[0]["y"] = yOrigin - (inArray[0]["height"] / 2);
+    var word;
+    var dir;
     for (var i = 1; i < inArray.length; i++) {
-        var word = inArray[i];
-        word["x"] = xOrigin - (word["width"]/2);
-        word["y"] = yOrigin - (word["height"]/2);
-        var dir = Math.floor(Math.random() * 8);
-        var y = 0;
-        var x = 0;
-        if(dir >= 5){
-            y = -1;
-        }else if(dir <= 2){
-            y = 1;
-        }
-        if(dir == 0 || dir == 7){
-            x = 0;
-        }else if(dir%2 == 0){
-            x = 1;
-        }else{
-            x = -1;
-        }
-        for(var j = 0; j < i; j++){
-            while(overlap(word, inArray[j])){
-                word["y"] -= y;
-                word["x"] -= x;
+        word = inArray[i];
+        word["x"] = xOrigin - (word["width"] / 2);
+        word["y"] = yOrigin - (word["height"] / 2);
+        dir = randDir();
+        for (var j = 0; j < i; j++) {
+            while (overlap(word, inArray[j])) {
+                word["y"] += dir["y"];
+                word["x"] += dir["x"];
             }
         }
     }
     for (var i = 0; i < inArray.length; i++) {
-            inArray[i]["x"] = inArray[i]["x"]+"px";
-            inArray[i]["y"] = inArray[i]["y"]+"px";
-        }
-    
+        inArray[i]["x"] = inArray[i]["x"] + "px";
+        inArray[i]["y"] = inArray[i]["y"] + "px";
+    }
+
     return inArray;
 }
 
-function overlap(ind1, ind2){
-    var fudge = 0;
+function randDir(){
+    var x = 0;
+    var y = 0;
+    var output = [];
+    var dir = Math.floor(Math.random()*4);
+    if(dir >= 2){
+        x = 1;
+    }else{
+        x = -1;
+    }
     
+    if(dir % 2 == 0){
+        y = 1;
+    }else{
+        y = -1;
+    }
+    output["x"] = x;
+    output["y"] = y;
+    return output;
+}
+function overlap(ind1, ind2) {
+    var fudge = 1;
+
     var top1 = ind1["y"];
     var lft1 = ind1["x"];
     var bot1 = ind1["y"] + ind1["height"];
     var rgt1 = ind1["x"] + ind1["width"];
-    
+
     var top2 = ind2["y"] - fudge;
     var lft2 = ind2["x"] - fudge;
     var bot2 = ind2["y"] + ind2["height"] + fudge;
     var rgt2 = ind2["x"] + ind2["width"] + fudge;
-    
-    if(bot1 < top2 || top1 > bot2 || rgt1 < lft2 || lft1 > rgt2){
+
+    if (bot1 < top2 || top1 > bot2 || rgt1 < lft2 || lft1 > rgt2) {
         return false;
-    }else{
+    }
+    else {
         return true;
     }
 }
@@ -140,8 +161,8 @@ function write(inArray, loc) {
         var line = "";
         line += "<span class='cloudWord' id='cloud_" + wordObj['word'] + "' style ='";
         line += "font-size:" + wordObj["font-size"] + "; ";
-        line += "height:"+wordObj["height"]+"px;";
-        line += "line-height:"+wordObj["height"]+"px;";
+        line += "height:" + wordObj["height"] + "px;";
+        line += "line-height:" + wordObj["height"] + "px;";
         line += "top:" + wordObj["y"] + "; ";
         line += "left:" + wordObj["x"] + "; ";
         line += "'>" + wordObj['word'] + "</span> \n";
@@ -149,4 +170,14 @@ function write(inArray, loc) {
         markup += line;
     }
     $(loc).html(markup);
+}
+
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
 }
